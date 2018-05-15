b835e94391c784cfb51243b6a8f2ee95ae4c4141
neghao <neghaoimac@34.39.17.218.broad.sz.gd.dynamic.163data.com.cn>
2018-05-15 11:12:43 +0800

    add uuid udid

---
+++ UDID_Tools/NegHUUIDTool.h
@@ -0,0 +1,15 @@
+//
+//  NegHUUIDTool.h
+//  appKeyChain
+//
+//  Created by NegHao.W on 16/3/10.
+//  Copyright © 2016年 NegHao.W. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NegHUUIDTool : NSObject
+
++(NSString *)UUID;
+
+@end
